6d224f44155cba7b1637f06885b7f1d4621ebdc6
esthugo <105492283+esthugo@users.noreply.github.com>
2022-05-13 11:38:56 +0100

    Add files via upload

---
+++ teste/app/src/main/java/com/example/teste/SingUpActivity.java
@@ -0,0 +1,15 @@
+package com.example.teste;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+
+        getSupportActionBar().hide();
+    }
+}
